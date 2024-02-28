c54c17b58e11c8cd010e01a153e258a53dd60782
nachocua <nachocuatrin@gmail.com>
2024-02-28 14:19:56 -0300

    Update V. Gráfica

--- Gestor de rotaciones/Form1.Designer.cs
+++ Gestor de rotaciones/Form1.Designer.cs
@@ -44,42 +44,53 @@
             this.clbDíasSemana = new System.Windows.Forms.CheckedListBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dtpVacacionesInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtpVacacionesFin = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbOcultarFeriados = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nupCantidadRotaciones)).BeginInit();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dtpFechaDesde
             // 
             this.dtpFechaDesde.CustomFormat = "";
-            this.dtpFechaDesde.Location = new System.Drawing.Point(16, 62);
+            this.dtpFechaDesde.Location = new System.Drawing.Point(21, 76);
+            this.dtpFechaDesde.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpFechaDesde.Name = "dtpFechaDesde";
-            this.dtpFechaDesde.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaDesde.Size = new System.Drawing.Size(265, 22);
             this.dtpFechaDesde.TabIndex = 0;
             // 
             // dtpFechaHasta
             // 
-            this.dtpFechaHasta.Location = new System.Drawing.Point(317, 62);
+            this.dtpFechaHasta.Location = new System.Drawing.Point(423, 76);
+            this.dtpFechaHasta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpFechaHasta.Name = "dtpFechaHasta";
-            this.dtpFechaHasta.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaHasta.Size = new System.Drawing.Size(265, 22);
             this.dtpFechaHasta.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(79, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Fecha Inicio";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(314, 30);
+            this.label2.Location = new System.Drawing.Point(419, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.Size = new System.Drawing.Size(138, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Fecha de Finalización";
             // 
@@ -89,16 +100,19 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.dtpFechaHasta);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(533, 102);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(711, 126);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Intervalo de clases";
             // 
             // nupCantidadRotaciones
             // 
-            this.nupCantidadRotaciones.Location = new System.Drawing.Point(317, 69);
+            this.nupCantidadRotaciones.Location = new System.Drawing.Point(423, 85);
+            this.nupCantidadRotaciones.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nupCantidadRotaciones.Maximum = new decimal(new int[] {
             6,
             0,
@@ -110,7 +124,7 @@
             0,
             0});
             this.nupCantidadRotaciones.Name = "nupCantidadRotaciones";
-            this.nupCantidadRotaciones.Size = new System.Drawing.Size(66, 20);
+            this.nupCantidadRotaciones.Size = new System.Drawing.Size(88, 22);
             this.nupCantidadRotaciones.TabIndex = 9;
             this.nupCantidadRotaciones.Value = new decimal(new int[] {
             1,
@@ -121,9 +135,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 69);
+            this.label3.Location = new System.Drawing.Point(17, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.Size = new System.Drawing.Size(35, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "Días";
             // 
@@ -131,20 +146,25 @@
             // 
             this.lbListaFeriados.Enabled = false;
             this.lbListaFeriados.FormattingEnabled = true;
-            this.lbListaFeriados.Location = new System.Drawing.Point(59, 19);
+            this.lbListaFeriados.ItemHeight = 16;
+            this.lbListaFeriados.Location = new System.Drawing.Point(8, 23);
+            this.lbListaFeriados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbListaFeriados.Name = "lbListaFeriados";
-            this.lbListaFeriados.Size = new System.Drawing.Size(157, 121);
+            this.lbListaFeriados.Size = new System.Drawing.Size(208, 148);
             this.lbListaFeriados.TabIndex = 11;
             this.lbListaFeriados.SelectedIndexChanged += new System.EventHandler(this.lbListaFeriados_SelectedIndexChanged);
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.cbOcultarFeriados);
             this.groupBox3.Controls.Add(this.btnQuitarFeriado);
             this.groupBox3.Controls.Add(this.btnAgregarFeriado);
             this.groupBox3.Controls.Add(this.lbListaFeriados);
-            this.groupBox3.Location = new System.Drawing.Point(11, 263);
+            this.groupBox3.Location = new System.Drawing.Point(16, 325);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(533, 150);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(380, 185);
             this.groupBox3.TabIndex = 12;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Feriados";
@@ -152,9 +172,10 @@
             // btnQuitarFeriado
             // 
             this.btnQuitarFeriado.Enabled = false;
-            this.btnQuitarFeriado.Location = new System.Drawing.Point(317, 89);
+            this.btnQuitarFeriado.Location = new System.Drawing.Point(237, 91);
+            this.btnQuitarFeriado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnQuitarFeriado.Name = "btnQuitarFeriado";
-            this.btnQuitarFeriado.Size = new System.Drawing.Size(154, 23);
+            this.btnQuitarFeriado.Size = new System.Drawing.Size(132, 28);
             this.btnQuitarFeriado.TabIndex = 13;
             this.btnQuitarFeriado.Text = "Quitar Feriado";
             this.btnQuitarFeriado.UseVisualStyleBackColor = true;
@@ -162,9 +183,10 @@
             // 
             // btnAgregarFeriado
             // 
-            this.btnAgregarFeriado.Location = new System.Drawing.Point(317, 36);
+            this.btnAgregarFeriado.Location = new System.Drawing.Point(237, 23);
+            this.btnAgregarFeriado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAgregarFeriado.Name = "btnAgregarFeriado";
-            this.btnAgregarFeriado.Size = new System.Drawing.Size(154, 23);
+            this.btnAgregarFeriado.Size = new System.Drawing.Size(132, 28);
             this.btnAgregarFeriado.TabIndex = 12;
             this.btnAgregarFeriado.Text = "Agregar Feriado";
             this.btnAgregarFeriado.UseVisualStyleBackColor = true;
@@ -172,9 +194,10 @@
             // 
             // btnGenerarFechas
             // 
-            this.btnGenerarFechas.Location = new System.Drawing.Point(76, 473);
+            this.btnGenerarFechas.Location = new System.Drawing.Point(101, 582);
+            this.btnGenerarFechas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGenerarFechas.Name = "btnGenerarFechas";
-            this.btnGenerarFechas.Size = new System.Drawing.Size(154, 23);
+            this.btnGenerarFechas.Size = new System.Drawing.Size(205, 28);
             this.btnGenerarFechas.TabIndex = 13;
             this.btnGenerarFechas.Text = "Generar fechas";
             this.btnGenerarFechas.UseVisualStyleBackColor = true;
@@ -182,9 +205,10 @@
             // 
             // rtbFechasFinales
             // 
-            this.rtbFechasFinales.Location = new System.Drawing.Point(331, 419);
+            this.rtbFechasFinales.Location = new System.Drawing.Point(441, 516);
+            this.rtbFechasFinales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtbFechasFinales.Name = "rtbFechasFinales";
-            this.rtbFechasFinales.Size = new System.Drawing.Size(154, 141);
+            this.rtbFechasFinales.Size = new System.Drawing.Size(204, 173);
             this.rtbFechasFinales.TabIndex = 14;
             this.rtbFechasFinales.Text = "";
             // 
@@ -197,9 +221,10 @@
             "Miercoles",
             "Jueves",
             "Viernes"});
-            this.clbDíasSemana.Location = new System.Drawing.Point(59, 26);
+            this.clbDíasSemana.Location = new System.Drawing.Point(79, 32);
+            this.clbDíasSemana.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clbDíasSemana.Name = "clbDíasSemana";
-            this.clbDíasSemana.Size = new System.Drawing.Size(89, 94);
+            this.clbDíasSemana.Size = new System.Drawing.Size(117, 106);
             this.clbDíasSemana.TabIndex = 15;
             // 
             // groupBox4
@@ -208,9 +233,11 @@
             this.groupBox4.Controls.Add(this.clbDíasSemana);
             this.groupBox4.Controls.Add(this.label3);
             this.groupBox4.Controls.Add(this.nupCantidadRotaciones);
-            this.groupBox4.Location = new System.Drawing.Point(12, 120);
+            this.groupBox4.Location = new System.Drawing.Point(16, 148);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(532, 137);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Size = new System.Drawing.Size(709, 169);
             this.groupBox4.TabIndex = 15;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Rotación";
@@ -218,30 +245,101 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(316, 26);
+            this.label4.Location = new System.Drawing.Point(421, 32);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.Size = new System.Drawing.Size(134, 16);
             this.label4.TabIndex = 16;
             this.label4.Text = "Rotaciones en el año";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dtpVacacionesFin);
+            this.groupBox2.Controls.Add(this.dtpVacacionesInicio);
+            this.groupBox2.Location = new System.Drawing.Point(404, 325);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(321, 185);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vacaciones";
+            // 
+            // dtpVacacionesInicio
+            // 
+            this.dtpVacacionesInicio.CustomFormat = "";
+            this.dtpVacacionesInicio.Location = new System.Drawing.Point(17, 59);
+            this.dtpVacacionesInicio.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpVacacionesInicio.Name = "dtpVacacionesInicio";
+            this.dtpVacacionesInicio.Size = new System.Drawing.Size(265, 22);
+            this.dtpVacacionesInicio.TabIndex = 1;
+            // 
+            // dtpVacacionesFin
+            // 
+            this.dtpVacacionesFin.Location = new System.Drawing.Point(17, 128);
+            this.dtpVacacionesFin.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpVacacionesFin.Name = "dtpVacacionesFin";
+            this.dtpVacacionesFin.Size = new System.Drawing.Size(265, 22);
+            this.dtpVacacionesFin.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 29);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Fecha Inicio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 97);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Fecha de Finalización";
+            // 
+            // cbOcultarFeriados
+            // 
+            this.cbOcultarFeriados.AutoSize = true;
+            this.cbOcultarFeriados.Checked = true;
+            this.cbOcultarFeriados.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbOcultarFeriados.Location = new System.Drawing.Point(246, 151);
+            this.cbOcultarFeriados.Name = "cbOcultarFeriados";
+            this.cbOcultarFeriados.Size = new System.Drawing.Size(117, 20);
+            this.cbOcultarFeriados.TabIndex = 14;
+            this.cbOcultarFeriados.Text = "Mostrar Todos";
+            this.cbOcultarFeriados.UseVisualStyleBackColor = true;
+            this.cbOcultarFeriados.CheckedChanged += new System.EventHandler(this.cbOcultarFeriados_CheckedChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 571);
+            this.ClientSize = new System.Drawing.Size(749, 703);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.rtbFechasFinales);
             this.Controls.Add(this.btnGenerarFechas);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nupCantidadRotaciones)).EndInit();
             this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -264,6 +362,12 @@
         private System.Windows.Forms.CheckedListBox clbDíasSemana;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpVacacionesFin;
+        private System.Windows.Forms.DateTimePicker dtpVacacionesInicio;
+        private System.Windows.Forms.CheckBox cbOcultarFeriados;
     }
 }
 
--- Gestor de rotaciones/Form1.cs
+++ Gestor de rotaciones/Form1.cs
@@ -7,25 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace Gestor_de_rotaciones
 {
     public partial class Form1 : Form
     {
+        List<DateTime> listaFeriados = new List<DateTime>();
         public Form1()
         {
             InitializeComponent();
             //dtpFechaDesde.Value = new DateTime(DateTime.Now.Year, 1, 1);
             //dtpFechaHasta.Value = new DateTime(DateTime.Now.Year, 12, 31);
+
+            //INTERVALO DE CLASES
             dtpFechaDesde.Value = new DateTime(DateTime.Now.Year, 3, 4);
-            dtpFechaHasta.Value = new DateTime(DateTime.Now.Year, 11, 18);
+            dtpFechaHasta.Value = new DateTime(DateTime.Now.Year, 5, 6);
+
+            //INTERVALO DE VACACIONES
+            dtpVacacionesInicio.Value = new DateTime(DateTime.Now.Year, 7, 8);
+            dtpVacacionesFin.Value = new DateTime(DateTime.Now.Year, 7, 19);
+
+            //FERIADOS DEL AÑO
+            DateTime feriados = new DateTime(DateTime.Now.Year, 3, 28);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 3, 29);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 4, 1);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 4, 2);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 4, 2);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 5, 1);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 6, 20);
+            listaFeriados.Add(feriados);
+            feriados = new DateTime(DateTime.Now.Year, 7, 9);
+            listaFeriados.Add(feriados);
+            AgregarFeriados(listaFeriados);
         }
         private void btnGenerarFechas_Click(object sender, EventArgs e)
         {
             bool state = false;
             int i = 0;
+            int cantidadDias = 0;
             List<int> diasElegidos = new List<int>();
-            int[] diasPorRotacion = null;
+            //DateTime fechaInicio;
+            //DateTime fechaFinal;
+            //int[] diasPorRotacion = null;
+            
+            //CALCULA QUE DÍAS A LA SEMANA SE SELECCIONÓ
             do
             {
                 if (clbDíasSemana.GetItemChecked(i))
@@ -35,23 +67,63 @@ namespace Gestor_de_rotaciones
                 }
                 i++;
             } while (i < clbDíasSemana.Items.Count);
+            
+            //CHEQUEA QUE HAYA DÍAS SELECCIONADOS
             if (!state)
             {
                 MessageBox.Show("Debes seleccionar al menos 1 día de rotaciones");
             }
             else
             {
-                int cantidadDias = 0;
+                rtbFechasFinales.Clear();
+                List<DateTime> listaFeriadosAChequear = new List<DateTime>();
+                for (i = 0; i < listaFeriados.Count; i++)
+                {
+                    if (diasElegidos.IndexOf((int)listaFeriados[i].DayOfWeek) != -1)
+                    {
+                        listaFeriadosAChequear.Add(listaFeriados[i]);
+                    }
+                }
+                foreach (DateTime dt in listaFeriadosAChequear)
+                {
+                    rtbFechasFinales.Text = dt.ToString();
+                }
+                /*
+                foreach (DateTime dt in listaFeriados)
+                {
+                    listaFeriadosAChequear.Add(dt);
+                }
+                for (i = 0; i < listaFeriadosAChequear.Count; i++)
+                {
+                    if (diasElegidos.IndexOf((int)listaFeriadosAChequear[i].DayOfWeek) == -1)
+                    {
+                        listaFeriadosAChequear.RemoveAt(i);
+                        i--;
+                    }
+                }
+                */
+                cantidadDias -= listaFeriadosAChequear.Count;
+                rtbFechasFinales.Text = cantidadDias.ToString();
+                /*
                 for (DateTime fechaActual = dtpFechaDesde.Value; fechaActual <= dtpFechaHasta.Value; fechaActual = fechaActual.AddDays(1))
                 {
                     if (diasElegidos.IndexOf((int)fechaActual.DayOfWeek) != -1)
                     {
-                        if (lbListaFeriados.Items.IndexOf(fechaActual.ToShortDateString()) == -1)
+                        i = 0;
+                        state = false;
+                        while (i < listaFeriados.Count && !state)
                         {
-                            cantidadDias++;
+                            if (lbListaFeriados.Items.IndexOf(fechaActual.ToShortDateString()) == -1)
+                            {
+                                cantidadDias++;
+                            }
+                            i++;
                         }
                     }
                 }
+                rtbFechasFinales.Text = cantidadDias.ToString();
+                */
+                /*
                 diasPorRotacion = new int[Convert.ToInt32(nupCantidadRotaciones.Value)];
                 for (i = 0; i < diasPorRotacion.Length; i++)
                 {
@@ -62,17 +134,46 @@ namespace Gestor_de_rotaciones
                     diasPorRotacion[i % diasPorRotacion.Length]++;
                 }
                 rtbFechasFinales.Clear();
-                DateTime fechaInicio = dtpFechaDesde.Value;
+                fechaInicio = dtpFechaDesde.Value;
                 while (diasElegidos.IndexOf((int)fechaInicio.DayOfWeek) != -1)
                 {
                     fechaInicio = fechaInicio.AddDays(1);
                 }
-                DateTime fechaFinal = fechaInicio.AddDays(1);
-                rtbFechasFinales.Text = fechaInicio.ToShortDateString() + " - " + fechaFinal.ToShortDateString();
-                for (i = 0; i < (int)nupCantidadRotaciones.Value - 1; i++)
+                fechaFinal = fechaInicio.AddDays(1);
+                i = 0;
+                while (i < diasPorRotacion[0])
+                {
+                    if (diasElegidos.IndexOf((int)fechaFinal.DayOfWeek) != -1)
+                    {
+                        i++;
+                    }
+                    fechaFinal = fechaFinal.AddDays(1);
+                }
+                fechaFinal = fechaFinal.AddDays(-1)
+                rtbFechasFinales.Text = diasPorRotacion[0] + " - " + fechaInicio.ToShortDateString() + " - " + fechaFinal.ToShortDateString();
+                */
+                /*
+                for (int j = 1; j < diasPorRotacion.Length; j++)
                 {
                     fechaInicio = fechaFinal.AddDays(1);
+                    while (diasElegidos.IndexOf((int)fechaInicio.DayOfWeek) != -1)
+                    {
+                        fechaInicio = fechaInicio.AddDays(1);
+                    }
+                    fechaFinal = fechaInicio.AddDays(1);
+                    i = 0;
+                    while (i < diasPorRotacion[0])
+                    {
+                        if (diasElegidos.IndexOf((int)fechaFinal.DayOfWeek) != -1)
+                        {
+                            i++;
+                        }
+                        fechaFinal = fechaFinal.AddDays(1);
+                    }
+                    fechaFinal = fechaFinal.AddDays(-1);
+                    rtbFechasFinales.Text += "\n" + diasPorRotacion[j] + " - " + fechaInicio.ToShortDateString() + " - " + fechaFinal.ToShortDateString();
                 }
+                */
             }
         }
 
@@ -83,6 +184,7 @@ namespace Gestor_de_rotaciones
             if (VentanaNuevoFeriado.ShowDialog() == DialogResult.OK)
             {
                 lbListaFeriados.Items.Add(VentanaNuevoFeriado.dtpFechaFeriado.Value.ToShortDateString());
+                listaFeriados.Add(VentanaNuevoFeriado.dtpFechaFeriado.Value);
             }
             VentanaNuevoFeriado.Dispose();
         }
@@ -103,6 +205,50 @@ namespace Gestor_de_rotaciones
             if (lbListaFeriados.SelectedIndex != -1)
             {
                 btnQuitarFeriado.Enabled = true;
+            }
+        }
+
+        private void cbOcultarFeriados_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cbOcultarFeriados.Checked)
+            {
+                List<int> diasElegidos = new List<int>();
+                int i = 0;
+                //CALCULA QUE DÍAS A LA SEMANA SE SELECCIONÓ
+                do
+                {
+                    if (clbDíasSemana.GetItemChecked(i))
+                    {
+                        diasElegidos.Add(i + 1);
+                    }
+                    i++;
+                } while (i < clbDíasSemana.Items.Count);
+                List<DateTime> listaFeriadosAChequear = new List<DateTime>();
+                foreach (DateTime dt in listaFeriados)
+                {
+                    listaFeriadosAChequear.Add(dt);
+                }
+                for (i = 0; i < listaFeriadosAChequear.Count; i++)
+                {
+                    if (diasElegidos.IndexOf((int)listaFeriadosAChequear[i].DayOfWeek) == -1)
+                    {
+                        listaFeriadosAChequear.RemoveAt(i);
+                        i--;
+                    }
+                }
+                AgregarFeriados(listaFeriadosAChequear);
+            }
+            else
+            {
+                AgregarFeriados(listaFeriados);
+            }
+        }
+        private void AgregarFeriados(List<DateTime> listaFeriados)
+        {
+            lbListaFeriados.Items.Clear();
+            foreach (DateTime dt in listaFeriados)
+            {
+                lbListaFeriados.Items.Add(dt);
             }
         }
     }
